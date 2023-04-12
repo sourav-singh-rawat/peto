9e0b799e5b34b16b048c8a43ef0f42a1753bcf4d
sourav-singh-rawat <souravrawat1999@gmail.com>
2023-04-12 15:25:43 +0530

    Add auth module

---
+++ lib/modules/data/auth/auth.dart
@@ -0,0 +1,68 @@
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:peto/modules/domain/auth/auth.dart';
+import 'package:peto/utils/app_extensions.dart';
+import 'package:peto/utils/profile/cubit/profile_cubit.dart';
+
+class KAuthImpl implements KAuth {
+  AuthStatus _authStatus = AuthStatus.unauthenticated;
+
+  @override
+  Future<void> bootUp() async {
+    log('[Auth.bootUp]');
+
+    _authStatus = AuthStatus.unauthenticated;
+
+    final token = await KAppX.storage.retrieve(
+      key: KAuth.authenticationTokenKey,
+      decoder: (token) {
+        return token;
+      },
+    );
+
+    if (token != null) {
+      _authStatus = AuthStatus.authenticated;
+
+      await KAppX.profile.refresh();
+    }
+  }
+
+  @override
+  void bootDown() {
+    log('[Auth.onBootDown]');
+  }
+
+  @override
+  void onBootUp() {
+    // TODO: implement onBootUp
+  }
+
+  @override
+  AuthStatus get authStatus => _authStatus;
+
+  @override
+  Future<void> onLogIn(String token) async {
+    await KAppX.storage.store<String>(
+      key: KAuth.authenticationTokenKey,
+      data: token,
+      encoder: (token) {
+        return token;
+      },
+      overwrite: true,
+    );
+
+    _authStatus = AuthStatus.authenticated;
+  }
+
+  @override
+  Future<void> onLogOut() async {
+    await KAppX.storage.delete(
+      key: KAuth.authenticationTokenKey,
+    );
+
+    _authStatus = AuthStatus.unauthenticated;
+
+    await KAppX.profile.refresh();
+  }
+}
---
+++ lib/modules/domain/auth/auth.dart
@@ -0,0 +1,18 @@
+import 'package:peto/modules/data/auth/auth.dart';
+import 'package:peto/modules/domain/modules.dart';
+
+enum AuthStatus { authenticated, unauthenticated }
+
+typedef OnAuthStatusChanged = void Function(AuthStatus authStatus);
+
+abstract class KAuth implements KModules<void> {
+  static final KAuth instance = KAuthImpl();
+
+  static const String authenticationTokenKey = 'authentication_token';
+
+  AuthStatus get authStatus;
+
+  Future<void> onLogIn(String token);
+
+  Future<void> onLogOut();
+}
