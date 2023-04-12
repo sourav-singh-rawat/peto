817393625237cee71a2bf5bc77049a82ab4e66dd
sourav-singh-rawat <souravrawat1999@gmail.com>
2023-04-12 15:55:43 +0530

    Add theme dependency injection

--- lib/utils/app_extensions.dart
+++ lib/utils/app_extensions.dart
@@ -1,5 +1,6 @@
 import 'package:peto/modules/domain/storage/storage.dart';
-import 'package:peto/utils/profile/cubit/profile_cubit.dart';
+import 'package:peto/utils/profile/profile_cubit.dart';
+import 'package:peto/utils/theme/theme_cubit.dart';
 
 class KAppX {
   KAppX._();
@@ -7,4 +8,6 @@ class KAppX {
   static KStorage storage = KStorage();
 
   static KProfileCubit profile = KProfileCubit();
+
+  static KThemeCubit theme = KThemeCubit();
 }
