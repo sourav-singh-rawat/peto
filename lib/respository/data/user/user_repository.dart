57eee78dbd78f05c71b6e8ccf42f66100832b679
sourav-singh-rawat <souravrawat1999@gmail.com>
2023-04-13 07:15:48 +0530

    Add user-details network call

--- lib/respository/data/user/user_repository.dart
+++ lib/respository/data/user/user_repository.dart
@@ -1,5 +1,11 @@
 import 'package:peto/respository/domain/user/user_repository.dart';
+import 'package:peto/respository/repository_helper_abstract.dart';
 
 part 'end_points.dart';
 
-class UserRepositoryImpl implements UserRepository {}
+class UserRepositoryImpl implements UserRepository {
+  @override
+  Future<Result<UserDetailsSuccess, UserDetailsFailure>> userDetails() {
+    throw UnimplementedError();
+  }
+}
