12fa63f47298aa8f1d9dd948afcffeceb3229d8a
sourav-singh-rawat <souravrawat1999@gmail.com>
2023-04-13 09:44:23 +0530

    Add route observer file. Commented for now

---
+++ lib/modules/data/router/observer.dart
@@ -0,0 +1,33 @@
+// part of 'router.dart';
+
+// class RouteAwareWidget extends StatefulWidget {
+//   State<RouteAwareWidget> createState() => RouteAwareWidgetState();
+// }
+
+// // Implement RouteAware in a widget's state and subscribe it to the RouteObserver.
+// class RouteAwareWidgetState extends State<RouteAwareWidget> with RouteAware {
+//   @override
+//   void didChangeDependencies() {
+//     super.didChangeDependencies();
+//     routeObserver.subscribe(this, ModalRoute.of(context) as PageRoute);
+//   }
+
+//   @override
+//   void dispose() {
+//     routeObserver.unsubscribe(this);
+//     super.dispose();
+//   }
+
+//   @override
+//   void didPush() {
+//     // Route was pushed onto navigator and is now topmost route.
+//   }
+
+//   @override
+//   void didPopNext() {
+//     // Covering route was popped off the navigator.
+//   }
+
+//   @override
+//   Widget build(BuildContext context) => Placeholder();
+// }
