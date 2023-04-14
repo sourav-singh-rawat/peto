65cb70c109c90f4b22634818e1e17a776b24b0f5
sourav-singh-rawat <souravrawat1999@gmail.com>
2023-04-14 11:25:35 +0530

    Add image and font assets

---
+++ lib/utils/assets/icons.dart
@@ -0,0 +1,6 @@
+class KIcons {
+  KIcons._();
+
+  static const google_logo = 'assets/icons/google-logo.png';
+  static const adopted_stamp = 'assets/icons/adopted-stamp.png';
+}
---
+++ lib/utils/assets/images.dart
@@ -0,0 +1,6 @@
+class KImages {
+  KImages._();
+
+  static const logo = 'assets/images/logo.png';
+  static const login_page_image = 'assets/images/login-image.png';
+}
