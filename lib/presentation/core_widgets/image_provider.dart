53b9a3bc61eded17456d0ba3c294b8c1d6001bd3
sourav-singh-rawat <souravrawat1999@gmail.com>
2023-04-14 10:26:14 +0530

    Add image provider custom wrapper

---
+++ lib/presentation/core_widgets/image_provider.dart
@@ -0,0 +1,75 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+class KImageProvider extends StatelessWidget {
+  final String imageUrl;
+  final Color? backgroundColor;
+  final Duration? fadeInDuration;
+  final double? width;
+  final double? height;
+  final BoxFit? fit;
+  final BorderRadius? borderRadius;
+
+  const KImageProvider(
+    this.imageUrl, {
+    Key? key,
+    this.backgroundColor,
+    this.fadeInDuration = const Duration(milliseconds: 200),
+    this.width,
+    this.height,
+    this.fit,
+    this.borderRadius,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final widget = CachedNetworkImage(
+      cacheKey: imageUrl,
+      imageUrl: imageUrl,
+      fadeInDuration: fadeInDuration!,
+      fit: fit ?? BoxFit.cover,
+      width: width,
+      height: height,
+      imageBuilder: (context, imageProvider) => Image(
+        image: imageProvider,
+        fit: fit ?? BoxFit.cover,
+        width: width,
+        height: height,
+      ),
+      placeholder: (context, url) {
+        return SizedBox.expand(
+          child: Container(
+            width: (height ?? 6) / 6,
+            height: (height ?? 6) / 6,
+            alignment: Alignment.center,
+            child: const Center(
+              child: CircularProgressIndicator(),
+            ),
+          ),
+        );
+      },
+      errorWidget: (context, error, stackTrace) {
+        return SizedBox.expand(
+          child: Container(
+            color: backgroundColor,
+            child: Center(
+              child: Icon(
+                Icons.image_not_supported,
+                size: (height ?? 6) / 6,
+              ),
+            ),
+          ),
+        );
+      },
+    );
+
+    if (borderRadius != null) {
+      return ClipRRect(
+        borderRadius: borderRadius,
+        child: widget,
+      );
+    } else {
+      return widget;
+    }
+  }
+}
