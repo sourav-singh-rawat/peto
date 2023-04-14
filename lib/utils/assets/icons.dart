5b0f6b5627784945b0aa85d583f21c10a537eb0c
sourav-singh-rawat <souravrawat1999@gmail.com>
2023-04-14 11:44:20 +0530

    Add icon assets

--- lib/utils/assets/icons.dart
+++ lib/utils/assets/icons.dart
@@ -3,4 +3,6 @@ class KIcons {
 
   static const google_logo = 'assets/icons/google-logo.png';
   static const adopted_stamp = 'assets/icons/adopted-stamp.png';
+  static const pen = 'assets/icons/pen.png';
+  static const pin_location = 'assets/icons/pin-location.png';
 }
