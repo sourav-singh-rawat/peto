ee50a2593976a8e36565223b500b2da701a6d32e
sourav-singh-rawat <souravrawat1999@gmail.com>
2023-04-14 17:15:11 +0530

    Add app_bar wrapper

---
+++ lib/presentation/core_widgets/app_bar.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:peto/utils/app_extensions.dart';
+
+class KAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final PreferredSizeWidget? bottom;
+  final Color? backgroundColor;
+  final Widget? title;
+  final Widget? leading;
+  final List<Widget>? actions;
+  final bool? centerTitle;
+  final VoidCallback? onPopCallback;
+  final double? elevation;
+  const KAppBar({
+    Key? key,
+    this.bottom,
+    this.backgroundColor,
+    this.title,
+    this.leading,
+    this.actions,
+    this.centerTitle,
+    this.onPopCallback,
+    this.elevation,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final ModalRoute<dynamic>? parentRoute = ModalRoute.of(context);
+    final canPop = parentRoute?.canPop ?? false;
+    final useCloseButton = parentRoute is PageRoute<dynamic> && parentRoute.fullscreenDialog;
+
+    return SafeArea(
+      child: AppBar(
+        leading: leading ??
+            () {
+              if (!canPop) {
+                return null;
+              }
+
+              return IconButton(
+                padding: EdgeInsets.zero,
+                icon: Icon(
+                  useCloseButton ? Icons.close : Icons.arrow_back,
+                  color: KAppX.theme.current.colors.onBackground,
+                  size: 28,
+                ),
+                onPressed: () {
+                  onPopCallback?.call();
+
+                  KAppX.router.pop();
+                },
+              );
+            }(),
+        backgroundColor: backgroundColor ?? KAppX.theme.current.colors.backgroundVariant,
+        elevation: elevation ?? 0,
+        title: title,
+        centerTitle: centerTitle,
+        actions: actions,
+        systemOverlayStyle: const SystemUiOverlayStyle(
+          statusBarColor: Colors.transparent,
+          systemNavigationBarColor: Colors.transparent,
+        ),
+        bottom: bottom ??
+            PreferredSize(
+              child: Container(
+                width: double.infinity,
+                color: const Color(0xffDFDFDF),
+              ),
+              preferredSize: const Size.fromHeight(1),
+            ),
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight((55) + (bottom?.preferredSize.height ?? 1));
+}
