da4edce21b1cceacd1bb84a856acf55b7d828902
sourav-singh-rawat <souravrawat1999@gmail.com>
2023-04-14 17:14:28 +0530

    Add scaffold wrapper

---
+++ lib/presentation/core_widgets/scaffold.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:peto/utils/app_extensions.dart';
+
+class KScaffold extends StatelessWidget {
+  final Widget? body;
+  final Color? backgroundColor;
+  final Widget? bottomSheet;
+  final PreferredSize? appBar;
+  const KScaffold({
+    super.key,
+    this.backgroundColor,
+    this.appBar,
+    this.body,
+    this.bottomSheet,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: backgroundColor ?? KAppX.theme.current.colors.backgroundVariant,
+      appBar: appBar,
+      body: body,
+      bottomSheet: bottomSheet,
+    );
+  }
+}
