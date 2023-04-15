88a19c3a521570bc53149b750c1a7e903823f503
sourav-singh-rawat <souravrawat1999@gmail.com>
2023-04-15 15:27:26 +0530

    Minor fix

--- lib/presentation/screens/pet_details/view.dart
+++ lib/presentation/screens/pet_details/view.dart
@@ -32,74 +32,99 @@ class PetDetailsView extends StatelessWidget {
         BlocProvider(create: (_) => _PetDetailsCubit()),
         BlocProvider(create: (_) => _PetImagePreviewCubit()),
       ],
-      child: BlocBuilder<_PetDetailsCubit, _PetDetailsState>(
-        bloc: BlocProvider.of<_PetDetailsCubit>(context)..initState(),
-        builder: (context, state) {
-          final safePadding = MediaQuery.of(context).padding.top;
-
-          final stateController = BlocProvider.of<_PetDetailsCubit>(context);
-
-          return Scaffold(
-            backgroundColor: Colors.white,
-            appBar: state.isScrolledToTop
-                ? AppBar(
-                    leading: IconButton(
-                      onPressed: () {
-                        KAppX.router.pop();
-                      },
-                      icon: const Icon(Icons.close),
-                    ),
-                    centerTitle: true,
-                    title: Text(petDetails.name ?? ''),
-                  )
-                : null,
-            body: Stack(
-              fit: StackFit.expand,
-              children: [
-                SizedBox(
-                  height: MediaQuery.of(context).size.height,
-                  child: _PetImagePreview(
-                    pid: petDetails.pid,
-                    images: petDetails.imageUrl,
-                  ),
-                ),
-                Positioned.fill(
-                  child: SafeArea(
-                    child: SingleChildScrollView(
-                      controller: stateController.scrollController,
-                      child: Column(
-                        mainAxisSize: MainAxisSize.max,
-                        children: [
-                          Container(
-                            height: 325 - safePadding,
-                            color: Colors.transparent,
-                            child: const _Header(),
-                          ),
-                          _PetDetailedInfo(
-                            petDetails: petDetails,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            bottomSheet: Container(
-              color: Colors.white,
-              width: double.infinity,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 20,
-                ).copyWith(
-                  bottom: 24,
-                ),
-                child: const _AdoptMeButton(),
-              ),
-            ),
-          );
-        },
+      child: _PetDetailsViewBody(
+        petDetails: petDetails,
       ),
+    );
+  }
+}
+
+class _PetDetailsViewBody extends StatefulWidget {
+  final PetDetails petDetails;
+  const _PetDetailsViewBody({
+    super.key,
+    required this.petDetails,
+  });
+
+  @override
+  State<_PetDetailsViewBody> createState() => __PetDetailsViewBodyState();
+}
+
+class __PetDetailsViewBodyState extends State<_PetDetailsViewBody> {
+  @override
+  void initState() {
+    BlocProvider.of<_PetDetailsCubit>(context).initState();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final safePadding = MediaQuery.of(context).padding.top;
+
+    final stateController = context.read<_PetDetailsCubit>();
+
+    return BlocBuilder<_PetDetailsCubit, _PetDetailsState>(
+      builder: (context, state) {
+        return Scaffold(
+          backgroundColor: Colors.white,
+          appBar: state.isScrolledToTop
+              ? AppBar(
+                  leading: IconButton(
+                    onPressed: () {
+                      KAppX.router.pop();
+                    },
+                    icon: const Icon(Icons.close),
+                  ),
+                  centerTitle: true,
+                  title: Text(widget.petDetails.name ?? ''),
+                )
+              : null,
+          body: Stack(
+            fit: StackFit.expand,
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height,
+                child: _PetImagePreview(
+                  pid: widget.petDetails.pid,
+                  images: widget.petDetails.imageUrl,
+                ),
+              ),
+              Positioned.fill(
+                child: SafeArea(
+                  child: SingleChildScrollView(
+                    controller: stateController.scrollController,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        Container(
+                          height: 325 - safePadding,
+                          color: Colors.transparent,
+                          child: const _Header(),
+                        ),
+                        _PetDetailedInfo(
+                          petDetails: widget.petDetails,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          bottomSheet: Container(
+            color: Colors.white,
+            width: double.infinity,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20,
+              ).copyWith(
+                bottom: 24,
+              ),
+              child: const _AdoptMeButton(),
+            ),
+          ),
+        );
+      },
     );
   }
 }
